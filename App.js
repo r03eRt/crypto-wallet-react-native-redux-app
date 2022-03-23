ff694388710b079f9572507c9c7b4e9d64ce4351
r03eRt <morgadoluengo@gmail.com>
2022-03-23 23:14:25 +0100

    Initial coomit with Navigation and dummy data and screens

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
+import Tabs from "./navigation/tabs";
+
+const Stack = createStackNavigator();
+
+export const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <Stack.Navigator
+            screenOptions={{
+                headerShown: false
+            }}
+            initialRouteName={'MainLayout'}
+        >
+            <Stack.Screen
+                name="MainLayout"
+                component={Tabs}
+            />
+        </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
